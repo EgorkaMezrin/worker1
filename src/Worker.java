abec7245f15b9488fd4f82eb034eaee9d1af2a8a
EgorkaMezrin <95707316+EgorkaMezrin@users.noreply.github.com>
2023-01-18 10:28:58 +0400

    Add files via upload

---
+++ src/Worker.java
@@ -0,0 +1,32 @@
+public class Worker {
+
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener listenerError) {
+        this.callback = callback;
+        this.errorCallback = listenerError;
+    }
+
+
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i == 33) {
+                errorCallback.onError("Task " + i + " is error");
+            } else {
+                callback.onDone("Task " + i + " is done");
+            }
+        }
+    }
+
+    @FunctionalInterface
+    public interface OnTaskDoneListener {
+        void onDone(String result);
+    }
+    @FunctionalInterface
+    public interface OnTaskErrorListener {
+        void onError(String result);
+    }
+}
